61728b6896998db324c70680c3578155ba94206a
chanindu <dspcdewnith@gmail.com>
2026-06-11 15:52:09 +0530

    created timer functionality

--- ios_development/ContentView.swift
+++ ios_development/ContentView.swift
@@ -1,5 +1,5 @@
 import SwiftUI
-
+import Combine
 
 
 
@@ -9,30 +9,40 @@ struct ContentView: View {
     @State private var timeLeft = 10
     @State private var buttonColor: Color = .blue
 
-   
-    
-
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+       
     var body: some View {
-      VStack(spacing: 30) {
-         Text("Score: \(score)")
-            .font(.largeTitle)
-            bold()
-         Text("Time: \(timeLeft)")
-            .font(.title)
-          Button(action: {
-          }) {
-            Text("TAP ME!")
-            .font(.largeTitle)
-            .frame(width: 200, height: 200)
-            .background(buttonColor)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-                        
-          }
-            }
-           
-            .padding()
-        }
+        VStack(spacing: 30) {
+
+                      Text("Score: \(score)")
+                          .font(.largeTitle)
+                          .bold()
+
+                      Text("Time: \(timeLeft)")
+                          .font(.title)
+
+                      Button(action: {
+
+                        }) {
+                          Text("TAP ME!")
+                              .font(.largeTitle)
+                              .frame(width: 200, height: 200)
+                              .background(buttonColor)
+                              .foregroundColor(.white)
+                              .clipShape(Circle())
+                             
+                              .animation(.easeInOut(duration: 0.3), value: timeLeft)
+                      }
+                  }
+                  .onReceive(timer) { _ in
+
+                     
+                          timeLeft -= 1
+                      }
+
+                      
+                  .padding()
+              }
     }
 
     
