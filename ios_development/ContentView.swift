a1982ce7799167952800b1a83511636b74344a33
chanindu <dspcdewnith@gmail.com>
2026-06-11 15:59:06 +0530

    Update timer functionality

--- ios_development/ContentView.swift
+++ ios_development/ContentView.swift
@@ -8,6 +8,9 @@ struct ContentView: View {
     @State private var score = 0
     @State private var timeLeft = 10
     @State private var buttonColor: Color = .blue
+    
+    @State private var isPlaying = false
+    @State private var gameOver = false
 
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
        
@@ -35,10 +38,17 @@ struct ContentView: View {
                       }
                   }
                   .onReceive(timer) { _ in
-
-                     
-                          timeLeft -= 1
+                      if isPlaying && timeLeft > 0 {
+                        timeLeft -= 1
                       }
+
+                     if timeLeft == 0 && isPlaying {
+                            isPlaying = false
+                            gameOver = true
+                    }
+                     
+                          
+                }
 
                       
                   .padding()
