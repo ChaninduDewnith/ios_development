ec5225ecb6bb847c4c89ea83cb413fd9d6957c0c
chanindu <dspcdewnith@gmail.com>
2026-06-11 14:54:05 +0530

    Initial Commit

---
+++ ios_development/ios_developmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_developmentApp.swift
+//  ios_development
+//
+//  Created by student2 on 2026-06-11.
+//
+
+import SwiftUI
+
+@main
+struct ios_developmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
