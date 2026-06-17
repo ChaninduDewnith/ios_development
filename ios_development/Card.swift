5992dfb2933fe60ce6a3a5fb944bb79868d5a05b
chanindu <dspcdewnith@gmail.com>
2026-06-17 11:53:51 +0530

    created card component

---
+++ ios_development/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  ios_development
+//
+//  Created by student2 on 2026-06-17.
+//
+
+import Foundation
+
+struct Card: Identifiable {
+    let id = UUID()
+    var isLit: Bool = false
+}
