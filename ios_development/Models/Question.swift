4be5697315c895ecaaf51b3289efba1b2af70806
dewnith <dspcdewnith@gmail.com>
2026-07-09 14:07:24 +0530

    Add question model file

---
+++ ios_development/Models/Question.swift
@@ -0,0 +1,38 @@
+//
+//  Question.swift
+//  ios_development
+//
+//  Created by cobsccomp251p-055 on 2026-07-09.
+//
+
+import Foundation
+
+struct QuizResponse: Codable {
+    let results: [Question]
+}
+
+struct Question: Codable, Identifiable {
+    
+    let id = UUID()
+    
+    let type: String
+    let difficulty: String
+    let category: String
+    let question: String
+    let correct_answer: String
+    let incorrect_answers: [String]
+    
+    enum CodingKeys: String, CodingKey {
+        case type
+        case difficulty
+        case category
+        case question
+        case correct_answer
+        case incorrect_answers
+    }
+    
+    var allAnswers: [String] {
+        (incorrect_answers + [correct_answer])
+    }
+    
+}
